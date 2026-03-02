1d3bbfeeb5556fe04f7b59bc8b32d1422c07085a
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-03-02 03:37:36 +0200

    Q_01

--- Assignment_05_PolyMorphism/Program.cs
+++ Assignment_05_PolyMorphism/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part_01
+
+            #region Q_01
+            //Q1 : What is the difference between static binding and dynamic binding? When does each one happen?
+            /*
+             
+            static binding  : used in Overloading, non-virtual methods    Happen in  Compile Time
+
+            dynamic binding : used in Overriding, interfaces  Happen in  Runtime
+            
+            */
+
+            #endregion
+
+            #endregion
         }
     }
 }
