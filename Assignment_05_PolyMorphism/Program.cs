1cb45ed3e2d06e31d37775a9efe779ef75c0cb96
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-03-02 03:26:15 +0200

    Add project files.

---
+++ Assignment_05_PolyMorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_05_PolyMorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
